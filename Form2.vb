3b918edad0018e66f2a235fccf3b1a904fe10609
marjuntadle064-pixel <marjuntadle064@gmail.com>
2025-10-24 21:13:54 +0800

    form3

---
+++ Form2.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form2
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+
+        Dim mainMenu As New Form1()
+        mainMenu.Show()
+        Me.Hide()
+
+    End Sub
+
+    Private Sub btnInputInfo_Click(sender As Object, e As EventArgs) Handles btnInputInfo.Click
+        Dim f As New Form3()
+        f.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnInputGrades_Click(sender As Object, e As EventArgs) Handles btnInputGrades.Click
+        Dim mainMenu As New Form4()
+        mainMenu.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim mainMenu As New Form5()
+        mainMenu.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ Form3.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtStudentID = New System.Windows.Forms.TextBox()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.txtFullName = New System.Windows.Forms.TextBox()
+        Me.cmbCourse = New System.Windows.Forms.ComboBox()
+        Me.dtpBirthday = New System.Windows.Forms.DateTimePicker()
+        Me.cmbYear = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.LoadData = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtStudentID
+        '
+        Me.txtStudentID.Location = New System.Drawing.Point(12, 34)
+        Me.txtStudentID.Name = "txtStudentID"
+        Me.txtStudentID.Size = New System.Drawing.Size(100, 20)
+        Me.txtStudentID.TabIndex = 0
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Location = New System.Drawing.Point(118, 34)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserID.TabIndex = 1
+        '
+        'txtFullName
+        '
+        Me.txtFullName.Location = New System.Drawing.Point(224, 34)
+        Me.txtFullName.Name = "txtFullName"
+        Me.txtFullName.Size = New System.Drawing.Size(158, 20)
+        Me.txtFullName.TabIndex = 2
+        '
+        'cmbCourse
+        '
+        Me.cmbCourse.FormattingEnabled = True
+        Me.cmbCourse.Items.AddRange(New Object() {"IT", "HRM", "COMPUTER ENGINERING", "EDUC"})
+        Me.cmbCourse.Location = New System.Drawing.Point(388, 34)
+        Me.cmbCourse.Name = "cmbCourse"
+        Me.cmbCourse.Size = New System.Drawing.Size(144, 21)
+        Me.cmbCourse.TabIndex = 3
+        '
+        'dtpBirthday
+        '
+        Me.dtpBirthday.Location = New System.Drawing.Point(538, 35)
+        Me.dtpBirthday.Name = "dtpBirthday"
+        Me.dtpBirthday.Size = New System.Drawing.Size(190, 20)
+        Me.dtpBirthday.TabIndex = 4
+        '
+        'cmbYear
+        '
+        Me.cmbYear.FormattingEnabled = True
+        Me.cmbYear.Items.AddRange(New Object() {"1 YEAR", "2 YEAR", "3 YEAR", "4 YEAR"})
+        Me.cmbYear.Location = New System.Drawing.Point(734, 35)
+        Me.cmbYear.Name = "cmbYear"
+        Me.cmbYear.Size = New System.Drawing.Size(144, 21)
+        Me.cmbYear.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Student ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "User ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(221, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Full name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(385, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Course"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(535, 18)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Birthday"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(731, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Year"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 79)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(716, 150)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(786, 312)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(92, 27)
+        Me.btnSave.TabIndex = 13
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'LoadData
+        '
+        Me.LoadData.Location = New System.Drawing.Point(12, 411)
+        Me.LoadData.Name = "LoadData"
+        Me.LoadData.Size = New System.Drawing.Size(92, 27)
+        Me.LoadData.TabIndex = 14
+        Me.LoadData.Text = "Load"
+        Me.LoadData.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(786, 345)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(92, 27)
+        Me.btnUpdate.TabIndex = 16
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(786, 378)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(92, 27)
+        Me.btnDelete.TabIndex = 17
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(786, 411)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 27)
+        Me.btnClear.TabIndex = 18
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(896, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.LoadData)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbYear)
+        Me.Controls.Add(Me.dtpBirthday)
+        Me.Controls.Add(Me.cmbCourse)
+        Me.Controls.Add(Me.txtFullName)
+        Me.Controls.Add(Me.txtUserID)
+        Me.Controls.Add(Me.txtStudentID)
+        Me.Name = "Form3"
+        Me.Text = "Student"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtStudentID As TextBox
+    Friend WithEvents txtUserID As TextBox
+    Friend WithEvents txtFullName As TextBox
+    Friend WithEvents cmbCourse As ComboBox
+    Friend WithEvents dtpBirthday As DateTimePicker
+    Friend WithEvents cmbYear As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnSave As Button
+    Friend WithEvents LoadData As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+End Class
